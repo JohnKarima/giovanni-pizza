b9c78b151685bde36e5a03b0a95cf1425074c5e4
John Karima <karimajohn24@gmail.com>
2020-08-03 17:40:37 +0300

    clear whitespace and make images accessibl on gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -11,30 +11,28 @@ function deliver() {
 function deliverer() {
   var yourName = prompt("What is your name? ")
   var yourAddress = prompt("What is your address? ")
-  if  (yourName.length === 0 || yourAddress.length === 0){
+  if (yourName.length === 0 || yourAddress.length === 0) {
     alert("Please enter your name and address to continue")
-  }else{
+  } else {
     alert("Thank you " + yourName + ", your order will be delivered to " + yourAddress)
-
-    }
-
   }
-  
-$(document).ready(function(){
-  $("form#pizzaForm").submit(function(event){
+}
+
+$(document).ready(function () {
+  $("form#pizzaForm").submit(function (event) {
     event.preventDefault();
     var pizzaSizeCost = parseInt($("input:radio[name=pizzaSize]:checked").val());
     var pizzaCrustCost = parseInt($("#pizzaCrust option:selected").val());
-    var pizzaToppingsCost1 = $.map($("input[name=toppings]:checked"), function(makeToppingsArray){return makeToppingsArray.value; })
+    var pizzaToppingsCost1 = $.map($("input[name=toppings]:checked"), function (makeToppingsArray) { return makeToppingsArray.value; })
     if (pizzaToppingsCost1.length === 0) {
       alert("You must select at least one topping");
     }
     var pizzaToppingsCost = pizzaToppingsCost1.map(Number);
-    var pizzaToppingsCostSum = pizzaToppingsCost.reduce(function(a, b){return a+b;})
+    var pizzaToppingsCostSum = pizzaToppingsCost.reduce(function (a, b) { return a + b; })
     var deliveryOption = parseInt($("input:radio[name=delivery]:checked").val());
     var pizzaCostTotal = pizzaSizeCost + pizzaCrustCost + pizzaToppingsCostSum + deliveryOption
     pizzacostarr.push(pizzaCostTotal)
     $("ul#pizzaOrderFinal").append("<li><span class='pizza-total-class'>" + pizzaCostTotal + "</span></li>");
     $('#pizzaForm')[0].reset();
   })
- })
+})
